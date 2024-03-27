9abae7659094e47dec0fa03046707a1372f9d2c9
BooZui <882itn@gmail.com>
2024-03-27 20:58:39 +0700

    nộp bài

--- source/practice/practice27-3/z.js
+++ source/practice/practice27-3/z.js
@@ -1,15 +1,16 @@
 let numbers = [
   [12, 4, 5, 9],
   [2, 5, 3, 2],
-  [1, 2, 1, 8]
+  [1, 2, 1, 8],
+  [3, 5, 6, 8]
 ];
 
 let sum = 0;
 
-numbers.forEach((array) => {
-  array.forEach((number) => {
-    number % 2 ==0 ? sum += number : sum += 0;
-  })
-});
+numbers = numbers.flat();
+
+for (let i = 0; i < numbers.length; i += 5) {
+  sum += numbers[i];
+}
 
 console.log(sum);
